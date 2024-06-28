2d5ae7568f45ef3aa5c6fc542575aee593cec720
cho-jae-seong <64570316+cho-jae-seong@users.noreply.github.com>
2024-06-28 08:25:22 +0000

    디지몬 Matthew 추가

--- src/components/cmn/Layout/ModalContainer.tsx
+++ src/components/cmn/Layout/ModalContainer.tsx
@@ -1,39 +1,38 @@
 import { ReactNode } from "react";
-import styled from '@emotion/styled'
-import {Rnd} from 'react-rnd'
+import styled from "@emotion/styled";
+import { Rnd } from "react-rnd";
 
 interface ModalsProviderProp {
-    children : ReactNode;
+    children: ReactNode;
 }
 
 const StyleRnd = styled(Rnd)`
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    .react-draggable{
-      position:relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .react-draggable {
+        position: relative;
     }
-    .react-resizable{
-      position:relative;
+    .react-resizable {
+        position: relative;
     }
-    `;
+`;
 
 const StyleContent = styled.div`
-    display:flex;
-    flex-direction:column;
-    height:100%;
-    background:gray;
-    padding:20px;
-    border-radius:8px;
-    `;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    background: white;
+    background-image: url("src/assets/Matthew.png");
+    padding: 20px;
+    border-radius: 8px;
+    border: 1px solid black;
+`;
 
-const ModalContainer = ({ children } : ModalsProviderProp) => {
-    
+const ModalContainer = ({ children }: ModalsProviderProp) => {
     return (
-        <StyleRnd default={{x:0,y:0,width:600,height:600}}>
-            <StyleContent>
-                {children}
-            </StyleContent>
+        <StyleRnd default={{ x: 0, y: -300, width: 600, height: 600 }}>
+            <StyleContent>{children}</StyleContent>
         </StyleRnd>
     );
 };
--- src/pages/smpl/poup/MyModal1.tsx
+++ src/pages/smpl/poup/MyModal1.tsx
@@ -1,29 +1,28 @@
-import useModals from 'hooks/cmn/useModals'
-import {modals} from 'components/organisms/Modals'
-import { useEffect, useState } from 'react';
+import useModals from "hooks/cmn/useModals";
+import { modals } from "components/cmn/Layout/Modals";
+import { useEffect, useState } from "react";
 
 interface Props {
-    onSubmit : () => void;
-    onClose  : () => void;
+    onSubmit: () => void;
+    onClose: () => void;
 }
 
-const MyModal = ({ onSubmit, onClose } : Props) => {
-
+const MyModal = ({ onSubmit, onClose }: Props) => {
     const { openModal } = useModals();
-    const [ text, setText] = useState("");
+    const [text, setText] = useState("");
 
     useEffect(() => {
-        console.log('전선배 렌더링');
+        console.log("전선배 렌더링");
 
         return () => {
-            console.log('전선배 다이');
-        }
-    },[]);
-    
+            console.log("전선배 다이");
+        };
+    }, []);
+
     const handleClickSubmit = () => {
         onSubmit();
     };
-    
+
     const handleClickCancel = () => {
         onClose();
     };
@@ -34,13 +33,18 @@ const MyModal = ({ onSubmit, onClose } : Props) => {
     };
 
     const openYoshiki = () => {
-        openModal(modals.myModal2, { onSubmit:()=>{alert('요시키상 어제 송심당에서 저녁먹음');}, foo: 'bar' });
-    }
+        openModal(modals.myModal2, {
+            onSubmit: () => {
+                alert("요시키상 어제 송심당에서 저녁먹음");
+            },
+            foo: "bar",
+        });
+    };
 
     return (
         <>
             <h1>전선배</h1>
-            <input type="text" value={text} onChange={onChange}/>
+            <input type="text" value={text} onChange={onChange} />
             <h2>이력 : </h2>
             <ul>
                 <li>NPDM 수행</li>
