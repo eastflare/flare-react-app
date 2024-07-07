8dc36ed4fce8f029ddca12653aca0126e545f767
eastflare <eastflare@outlook.com>
2024-07-08 01:58:34 +0900

    오늘은 이만

--- src/components/cmn/Layout/TopMenu.tsx
+++ src/components/cmn/Layout/TopMenu.tsx
@@ -1,23 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
 
 const TopMenu = () => {
   return (
-    <div className='topmenu'>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <StyledHeader>
+      <div className='topmenu'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </StyledHeader>
   );
 };
+
+const StyledHeader = styled.div`
+  display: flex;
+  height: 50px;
+  padding-right: 10px;
+  position: relative;
+  background-color: white;
+  border-bottom: 1px solid black;
+  transition: all 0.2s ease-out;
+`;
 
 export default TopMenu;
--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -1,7 +1,26 @@
-import MainRoutes from './MainRoutes';
+import MainRoutes from "./MainRoutes";
+import styled from "@emotion/styled";
 
 const MainRouter = () => {
-  return <MainRoutes />;
+  return (
+    <StyledMainContainer>
+      <MainRoutes />
+    </StyledMainContainer>
+  );
 };
+
+// const StyledGlobalContainer = styled.div`
+//   height: "inherit";
+//   min-height: "inherit";
+//   max-width: "100%";
+//   width: 100%;
+//   min-width: "800px";
+//   padding: 0;
+// `;
+
+const StyledMainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 export default MainRouter;
