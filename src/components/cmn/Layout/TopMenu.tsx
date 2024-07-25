8b3453e39ab22b193b121e1276df346c3c01eca5
eastflare <72382172+eastflare@users.noreply.github.com>
2024-07-25 01:47:36 +0000

    Home 으로 이동

--- src/components/cmn/Layout/TopMenu.tsx
+++ src/components/cmn/Layout/TopMenu.tsx
@@ -2,13 +2,24 @@ import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 
 const TopMenu = () => {
+  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    //TODO : 페이지 전체가 지원진다는 Confirm 창을 통해 실행 필요함
+
+    window.location.replace("/");
+  };
+
   return (
     <StyledHeader>
+      <button>좌측접기펴기</button>
       <div className='topmenu'>
         <nav>
           <ul>
             <li>
-              <Link to='/'>Home</Link>
+              <a href='/new-page' onClick={handleLinkClick}>
+                Home
+              </a>
             </li>
             <li>
               <Link to='/about'>About</Link>
