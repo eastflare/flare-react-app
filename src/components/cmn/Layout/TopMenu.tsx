045135ad09ee7370111468c312ed2773d0d53413
cho-jae-seong <64570316+cho-jae-seong@users.noreply.github.com>
2024-07-26 00:30:22 +0000

    GAP 구매요청

--- src/components/cmn/Layout/TopMenu.tsx
+++ src/components/cmn/Layout/TopMenu.tsx
@@ -5,22 +5,22 @@ const TopMenu = ({ onToggleLeftMenu }: { onToggleLeftMenu: () => void }) => {
   const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
 
-    //TODO : 페이지 전체가 지원진다는 Confirm 창을 통해 실행 필요함
+    // TODO: 페이지 전체가 지원진다는 Confirm 창을 통해 실행 필요함
 
     window.location.replace("/");
   };
 
   return (
     <StyledHeader>
-      <ToggleButton onClick={onToggleLeftMenu}>☰</ToggleButton>
-      <div className='topmenu'>
+      <LeftContainer>
+        <ToggleButton onClick={onToggleLeftMenu}>☰</ToggleButton>
+        <LogoContainer href='/' onClick={handleLinkClick}>
+          <img src='GAP.png' alt='Logo' />
+        </LogoContainer>
+      </LeftContainer>
+      <TopMenuItems>
         <nav>
           <ul>
-            <li>
-              <a href='/' onClick={handleLinkClick}>
-                Home
-              </a>
-            </li>
             <li>
               <Link to='/about'>About</Link>
             </li>
@@ -29,41 +29,83 @@ const TopMenu = ({ onToggleLeftMenu }: { onToggleLeftMenu: () => void }) => {
             </li>
           </ul>
         </nav>
-      </div>
+      </TopMenuItems>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.div`
   display: flex;
+  align-items: center;
   height: 50px;
-  padding-right: 10px;
-  position: relative;
+  padding: 0px;
   background-color: white;
   border-bottom: 1px solid black;
   transition: all 0.2s ease-out;
 `;
 
+const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 150px;
+`;
+
+const LogoContainer = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  img {
+    max-height: 100%;
+    max-width: 100%;
+  }
+`;
+
 const ToggleButton = styled.button`
-  background-color: #4caf50;
+  background-color: #2d9bb2;
   border: none;
   color: white;
-  padding: 10px 20px;
+  padding: 10px;
   text-align: center;
   text-decoration: none;
-  display: inline-block;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   font-size: 16px;
-  margin: 4px 2px;
+  width: 50px;
+  height: 50px;
   cursor: pointer;
   border-radius: 4px;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #45a049;
+    background-color: #2588a0;
   }
 
   &:active {
-    background-color: #3e8e41;
+    background-color: #1e7380;
+  }
+`;
+
+const TopMenuItems = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-start;
+  nav ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    li {
+      margin: 0 10px;
+      a {
+        text-decoration: none;
+        color: black;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
   }
 `;
 
