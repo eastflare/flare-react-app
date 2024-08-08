787f1c3ae5f2c32af51d0435af953653326e1f76
eastflare <72382172+eastflare@users.noreply.github.com>
2024-08-08 08:40:23 +0000

    환경설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import browserslistToEsbuild from "browserslist-to-esbuild";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),tsconfigPaths()],
-  define:{
-    "process.env":process.env,
-  },
-})
+export default ({ mode }: { mode: string }) => {
+  // Load app-level env vars to node-level env vars.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    optimizeDeps: {
+      include: ["@emotion/styled", "@mui/material/Tooltip"],
+    },
+    build: {
+      // browserslist leverage plugin
+      target: browserslistToEsbuild(),
+      outDir: "build",
+    },
+    server: {
+      port: 3000,
+    },
+    define: {
+      "process.env": env,
+    },
+    assetsInclude: ["**/*.xlsx"],
+    plugins: [tsconfigPaths(), react(), svgr()],
+  });
+};
