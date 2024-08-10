b3f681407a1f3fe873688a021d55c8ffa809f230
eastflare <72382172+eastflare@users.noreply.github.com>
2024-08-10 02:30:15 +0000

    sdklfjlds

--- src/provider/use-menu-init-effect.ts
+++ src/provider/use-menu-init-effect.ts
@@ -35,7 +35,7 @@ function useMenuInitEffect() {
         return;
       }
 
-      window.location.href = `http://sso.eastflare.iptime.org:8001/nls3/cookieLogin.jsp?UURL=${process.env.API_BASE_URL}&RTOA=1`;
+      window.location.href = `http://eastflare.iptime.org/login?UURL=${process.env.API_BASE_URL}&RTOA=1`;
     }
   };
 
