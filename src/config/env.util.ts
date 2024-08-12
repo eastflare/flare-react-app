59d320feaaf46659863da681f58328483646c9f4
cho-jae-seong <64570316+cho-jae-seong@users.noreply.github.com>
2024-08-12 04:45:24 +0000

    dddd

--- src/config/env.util.ts
+++ src/config/env.util.ts
@@ -11,12 +11,12 @@ export function isNumberString(input: string): input is string {
 export function readEnv<T extends string>({ envKey, typeGuard, typeLabel, defaultEnv }: { envKey: string; typeGuard?: TGuardFunc<T>; typeLabel?: string; defaultEnv?: T }) {
   const envInput = `${process.env[envKey]}` ?? defaultEnv;
   if (!envInput) {
-    throw new Error("[Env] ${envKey} 환경변수가 지정되지 않았습니다");
+    throw new Error(`[Env] ${envKey} 환경변수가 지정되지 않았습니다`);
   }
   if (typeGuard && typeLabel) {
     if (!typeGuard(envInput)) {
       console.error("Wrong envInput", envInput);
-      throw new Error("[Env] ${envKey} 환경변수가 ${typeLabel} 형식이어야 합니다");
+      throw new Error(`[Env] ${envKey} 환경변수가 ${typeLabel} 형식이어야 합니다`);
     }
     return envInput as T;
   } else {
