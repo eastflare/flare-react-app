c8c2614718733dc58576f0c5e16ba8dc3d93476d
eastflare <72382172+eastflare@users.noreply.github.com>
2024-08-13 08:58:23 +0000

    이거슨 불필요

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = app => {
-  app.use(
-    createProxyMiddleware("/api", {
-      target: "http://eastflare.iptime.org:8080",
-      changeOrigin: true,
-    })
-  );
-};
