03c83b05fd12360e4d340612ea02f5468eeeeb4d
cho-jae-seong <64570316+cho-jae-seong@users.noreply.github.com>
2024-08-14 01:00:45 +0000

    date type만 살려두기

--- src/utils/DateUtil.ts
+++ src/utils/DateUtil.ts
@@ -1,11 +1,6 @@
-//import { DateRangePickerValueType } from "@lges/design-system";
+import { DateRangePickerValueType } from "models/date/date-range-calendar";
 import dayjs from "dayjs";
 
-// export const isValidDateRangePickerValue = (dateRange: DateRangePickerValueType | unknown) => {
-//   return Array.isArray(dateRange) && dateRange.length === 2 && dateRange.every(date => dayjs(date).isValid()) && (dayjs(dateRange[0]).isSame(dayjs(dateRange[1])) || dayjs(dateRange[0]).isBefore(dayjs(dateRange[1])));
-// };
-
-//임시로 처리함
-export const isValidDateRangePickerValue = (dateRange: any) => {
+export const isValidDateRangePickerValue = (dateRange: DateRangePickerValueType | unknown) => {
   return Array.isArray(dateRange) && dateRange.length === 2 && dateRange.every(date => dayjs(date).isValid()) && (dayjs(dateRange[0]).isSame(dayjs(dateRange[1])) || dayjs(dateRange[0]).isBefore(dayjs(dateRange[1])));
 };
