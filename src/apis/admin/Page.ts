e637357c63955879740d450ac6baf8be2f173f96
eastflare <72382172+eastflare@users.noreply.github.com>
2024-08-19 04:08:44 +0000

    여기까지

--- src/apis/admin/Page.ts
+++ src/apis/admin/Page.ts
@@ -2,6 +2,18 @@ import { Page, PageCondition } from "models/admin/Page";
 import { CommonRequest, CommonResponse, DmlResponse, Method, ServiceName } from "models/common/RestApi";
 import { callApi } from "utils/ApiUtil";
 
+export const findPageById = async (pageId: string) => {
+  const request: CommonRequest = {
+    method: Method.GET,
+    url: "/v1/page/${pageId}",
+    serviceName: ServiceName.YOUR_BACK_END_SERVICE_NAME,
+    
+  };
+  const response: CommonResponse<Page> = await callApi(request);
+
+  return (response.successOrNot === "Y" ? response.data : null) as Page;
+};
+
 export const findPages = async (pageCondition: PageCondition) => {
   const request: CommonRequest = {
     method: Method.GET,
