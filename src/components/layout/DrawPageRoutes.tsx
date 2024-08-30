3f32a05a40d25e98cf16157d08da429c5f020021
cho-jae-seong <64570316+cho-jae-seong@users.noreply.github.com>
2024-08-30 00:36:29 +0000

    일단 커밋

--- src/components/layout/DrawPageRoutes.tsx
+++ src/components/layout/DrawPageRoutes.tsx
@@ -11,10 +11,11 @@ interface TaskRoutesProps {
 
 function DrawPageRoutes({ openedPageMap, routesProps, curPageId }: TaskRoutesProps) {
   //console.log("나는curRouteId입니다.test", curPageId);
+  console.log("이게 뭐하는 건가요?", openedPageMap);
   return (
     <>
       {Array.from(openedPageMap.entries()).map(([key, value]) => (
-        <DrawPageRoute key={key} {...value?.element?.props} routesProps={routesProps} pageItem={value} display={key === curPageId} />
+        <DrawPageRoute key={key} {...value} routesProps={routesProps} pageItem={value} display={key === curPageId} />
       ))}
     </>
   );
